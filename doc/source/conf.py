993c96ce567c3b6f572ebbee845509b2fc3be754
Geoffroy Desvernay <dgeo@centrale-med.fr>
2024-05-29 16:39:41 +0200

    version 1.4

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -57,7 +57,7 @@ author = u'Brandon Schneider and Peter Toth'
 # built documents.
 #
 # The short X.Y version.
-version = u'1.2'
+version = u'1.4'
 # The full version, including alpha/beta/rc tags.
 release = u'1.2'
 

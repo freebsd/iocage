9d6f4c90e9be3fe883c9cdc66692a32c4cce75a3
Michael Gmelin <freebsd@grem.de>
2026-06-08 17:10:24 +0200

    Fix command line parsing problems
    This adds a workaround that ensures flags that default to true
    and have no "off"-variant (like `--header` for "no headers" -
    yes, the irony) work with all versions of click (at least
    pre-8.2.0 through 8.4.1).
    
    Fixes #119

--- iocage_cli/df.py
+++ iocage_cli/df.py
@@ -35,8 +35,8 @@ import texttable
     "-h",
     "-H",
     is_flag=True,
-    default=True,
-    help="For scripting, use tabs for separators.")
+    default=not ioc.CLICK_WORKAROUND,
+    help="For scripting, no headers and tabs as separators.")
 @click.option(
     "--long",
     "-l",
@@ -53,6 +53,8 @@ import texttable
     help="Sorts the list by the given type")
 def cli(header, _long, _sort):
     """Allows a user to show resource usage of all jails."""
+    if ioc.CLICK_WORKAROUND:
+        header = not header
     table = texttable.Texttable(max_width=0)
     jail_list = ioc.IOCage().df()
 
--- iocage_cli/list.py
+++ iocage_cli/list.py
@@ -36,15 +36,15 @@ import iocage_lib.iocage as ioc
               flag_value="basejail", help="List all basejails.")
 @click.option("--template", "-t", "dataset_type", flag_value="template",
               help="List all templates.")
-@click.option("--header", "-h", "-H", is_flag=True, default=True,
-              help="For scripting, use tabs for separators.")
+@click.option("--header", "-h", "-H", is_flag=True, default=not ioc.CLICK_WORKAROUND,
+              help="For scripting, no headers and tabs as separators.")
 @click.option("--long", "-l", "_long", is_flag=True, default=False,
               help="Show the full uuid and ip4 address.")
 @click.option("--remote", "-R", is_flag=True,
               help="Show remote's available RELEASEs.")
 @click.option("--plugins", "-P", is_flag=True, help="Show available plugins.")
-@click.option("--http", default=True, help="Have --remote use HTTP instead.",
-              is_flag=True)
+@click.option("--http", is_flag=True, default=not ioc.CLICK_WORKAROUND,
+              help="Have --remote not use HTTP.")
 @click.option("--sort", "-s", "_sort", default="name", nargs=1,
               help="Sorts the list by the given type")
 @click.option("--quick", "-q", is_flag=True, default=False,
@@ -54,6 +54,10 @@ import iocage_lib.iocage as ioc
 def cli(dataset_type, header, _long, remote, http, plugins, _sort, quick,
         official):
     """This passes the arg and calls the jail_datasets function."""
+
+    if ioc.CLICK_WORKAROUND:
+        header = not header
+        http = not http
     freebsd_version = ioc_common.checkoutput(["freebsd-version"])
     iocage = ioc.IOCage(skip_jails=True)
 

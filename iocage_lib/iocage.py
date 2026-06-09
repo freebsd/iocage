96cc95929d438d477f3c866e49edbed154650e47
grembo <freebsd@grem.de>
2026-06-09 20:30:32 +0200

    Fix command line parsing problems (#122)
    This adds a workaround that ensures flags that default to true
    and have no "off"-variant (like `--header` for "no headers" -
    yes, the irony) work with all versions of click (at least
    pre-8.2.0 through 8.4.1).
    
    Fixes #119

--- iocage_lib/iocage.py
+++ iocage_lib/iocage.py
@@ -53,6 +53,13 @@ from iocage_lib.release import Release
 from iocage_lib.snapshot import SnapshotListableResource, Snapshot
 
 
+# Workaround for click bugs and incompatible changes introduced
+# in 8.2.x. Once we can upgrade to click 8.4.1, this and all
+# its consumers can be removed, but places using the workaround
+# need to be changed to use flag_value=False instead of
+# is_flag=True (and change default to True).
+CLICK_WORKAROUND=True
+
 class PoolAndDataset:
 
     def get_pool(self):
